989ced55de57283aabfa492755c6ebbe074a48a8
gw01720 <gw01720@rmn.com>
2020-09-06 22:15:10 -0500

    feat: google calendar integration with react scheduler

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,5 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../components/Page";
 
 export const Settings: React.FC = () => {
+  const userContext = useContext(UserContext);
+  console.log(userContext);
+
+  console.log(gapi);
+
+  if (gapi && gapi.auth2 && gapi.auth2.getAuthInstance().isSignedIn.get()) {
+    console.log(
+      gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile()
+    );
+  }
+
   return <>Settings</>;
 };
